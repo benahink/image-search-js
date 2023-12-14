cc0dd8a07e7836737becf8a98cb4bff629c17a85
benahink <benahink@gmail.com>
2023-12-13 21:34:32 -0700

    unsplash api fetching imgs

---
+++ app.js
@@ -0,0 +1,36 @@
+const accessKey = '9ZxquBwF0m84AqtRRIn-Wd9bAE_rm0onZdRuZ3dEtQ8'
+
+const searchForm = document.getElementById("search-form");
+const searchBox = document.getElementById("search-box");
+const searchResult = document.getElementById("search-result");
+const searchMoreBtn = document.getElementById("search-more-btn");
+
+let keyword = "";
+let page = 1;
+
+async function searchImages(){
+	keyword = searchBox.value;
+	const url = `https://api.unsplash.com/search/photos?page=${page}&query=${keyword}&client_id=${accessKey}&per_page=12`;
+
+	const response = await fetch(url);
+	const data = await response.json();
+
+	const results = data.results;
+
+	results.map(result => {
+		const image = document.createElement("img");
+		image.src = result.urls.small;
+		const imageLink = document.createElement("a");
+		imageLink.href = result.links.html;
+		imageLink.target = "_blank";
+
+		imageLink.appendChild(image);
+		searchResult.appendChild(imageLink);
+	})
+}
+
+searchForm.addEventListener("submit", (e) => {
+	e.preventDefault();
+	page = 1;
+	searchImages();
+})
